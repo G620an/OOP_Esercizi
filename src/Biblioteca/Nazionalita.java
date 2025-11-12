f1208bed30d35707edf2a5864ada0987f04b7b00
g620an <gabriele.iue2@gmail.com>
2025-11-12 17:07:18 +0100

    Autore e Nazionalità
    Signed-off-by: g620an <gabriele.iue2@gmail.com>

--- src/Biblioteca/Nazionalita.java
+++ src/Biblioteca/Nazionalita.java
@@ -1,5 +1,7 @@
 package Biblioteca;
 
+import java.util.Objects;
+
 public class Nazionalita{
     private String sig;
     private int ISO;
@@ -9,5 +11,37 @@ public class Nazionalita{
         }
         this.sig = sig;
         this.ISO = ISO;
+    }
+
+    public String getSig() {
+        return sig;
+    }
+
+    public int getISO() {
+        return ISO;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null){
+            return false;
+        }else if(o instanceof Nazionalita n){
+            return sig.equals(n.sig) && ISO == n.ISO;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sig, ISO);
+    }
+
+    @Override
+    public String toString() {
+        return "Nazionalita{" +
+                "sig='" + sig + '\'' +
+                ", ISO=" + ISO +
+                '}';
     }
 }
