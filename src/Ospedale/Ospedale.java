a2aef67583a4a6558645fd0d72f87c40631b65af
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-11-25 09:46:48 +0100

    update src/Ospedale/Ospedale.java

--- src/Ospedale/Ospedale.java
+++ src/Ospedale/Ospedale.java
@@ -1,3 +1,7 @@
-public interface Ospedale extends Iterable<Reparto>{
-  
+public interface Ospedale extends Iterable<Reparto>{ //Sara la nostra struttura dati
+  void aggiungiReparto(Reparto r)<
+  boolean rimuoviReparto(Reparto r)<
+  Reparto getReparto(int index);
+  double calcolaRatioMediciPazienti()<
+  boolean verificaStandard();
 }
