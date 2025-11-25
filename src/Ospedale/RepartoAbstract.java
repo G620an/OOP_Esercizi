0fe9e1658340ae8d66f65368611c02002973bd9b
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-11-25 09:39:23 +0100

    update src/Ospedale/RepartoAbstract.java

---
+++ src/Ospedale/RepartoAbstract.java
@@ -0,0 +1,24 @@
+public abstract class RepartoAbstract{
+  @Override
+  public String toString(){
+    return ""+ codiceReparto()+ " " + nomeReparto() + " " + capacitaPazienti() + " " + medici() + " " + superficie() + " " + criticità(); 
+  }
+  
+  @Override
+  public boolean equals(Object o){
+    if(o == this){
+      return true;
+    }
+    if(o == null){
+      return false;
+    }else if(o instanceof RepartoAbstract rep){
+      return this.codiceReparto().equals(rep.codiceReparto());
+    }
+    return false
+  }
+  
+  @Override
+  public int hashCode(){
+    return this.codiceReparto().hashCode();
+  }
+}
