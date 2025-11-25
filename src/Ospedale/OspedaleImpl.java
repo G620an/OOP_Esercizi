a01b1df5c124345cf6d95b39ed76ba0451c8c0bf
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-11-25 10:15:44 +0100

    update src/Ospedale/OspedaleImpl.java

---
+++ src/Ospedale/OspedaleImpl.java
@@ -0,0 +1,15 @@
+public class OspedaleImpl implements Ospedale{
+  public OspedaleImpl(){}
+  
+  @Override
+  public boolean rimuoviReparto(Reparto r){
+    
+  }
+  
+  @Override
+  public void aggiungiReparto(Reparto r){
+    
+  }
+  
+  
+}
