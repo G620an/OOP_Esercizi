084f00a7bfa4632550c0be23179dbd3e1152c538
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 11:59:01 +0100

    update src/Utile/utile.java

--- src/Utile/utile.java
+++ src/Utile/utile.java
@@ -5,14 +5,14 @@ import java.util.*;
 public class Utile{
     private Utile(){}
 
-    public static void generaRandom(OutputStream out, long n)throw new IOException(){
+    public static void generaRandom(OutputStream out, long n)throw new IOException{
        Random r = new Random();
        for(int i=0 ; i<n ; i++){
            out.writeLong(r.nextLong());
        }
     }
 
-    public static void naturalMergeSort(InputStream in)throw new IOException(){
+    public static void naturalMergeSort(InputStream in)throw new IOException{
         
     }
 
