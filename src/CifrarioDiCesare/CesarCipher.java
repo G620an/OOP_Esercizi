c852d88be41a762a3825c7099892f7f1abf46ec5
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 12:19:39 +0100

    update src/CifrarioDiCesare/CesarCipher.java

--- src/CifrarioDiCesare/CesarCipher.java
+++ src/CifrarioDiCesare/CesarCipher.java
@@ -5,9 +5,16 @@ public class CesarCipher{
   private CesarCipher(){};
   public static cifra(String path , int key){
     try(BufferedReader br = new  BufferedReader(new FileReader(path)), BufferedWriter bw = new BufferedWriter(new FileWriter(path))){
+      StringBuilder sb = new StringBuilder();
       Boolean continua = true;
       while(continua){
         String line = br.readLine();
+        if(line == null){
+          continua = false;
+          break;
+        }
+        char[] cl = line.toCharset();
+        
       }
     }catch(IOException e){
       e.printStackTrace();
