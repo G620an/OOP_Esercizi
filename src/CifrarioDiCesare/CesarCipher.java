90836baeccb3dc7507e1d50cfd340c32f461a89f
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 12:22:52 +0100

    update src/CifrarioDiCesare/CesarCipher.java

--- src/CifrarioDiCesare/CesarCipher.java
+++ src/CifrarioDiCesare/CesarCipher.java
@@ -16,7 +16,7 @@ public class CesarCipher{
         for(int i=0 ; i<line.length ; i++){
           line[i] =+ key;
         }
-        sb.append(line)
+        sb.append(line);
         
         
       }
