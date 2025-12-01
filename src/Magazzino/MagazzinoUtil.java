685b5a568044493de4da614fdac15a18d16ba656
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 13:02:18 +0100

    update src/Magazzino/MagazzinoUtil.java

--- src/Magazzino/MagazzinoUtil.java
+++ src/Magazzino/MagazzinoUtil.java
@@ -5,6 +5,10 @@ public class MagazzinoUtil{
   private MagazzinoUtil(){}
   
   public void salvaMagazzino(String path , Magazzino magazzino){
-    
+    try(BufferedWriter bw = new BufferedWriter(new FileWwriter(path))){
+      
+    }catch(IOException e){
+      e.printStackTrace();
+    }
   }
 }
