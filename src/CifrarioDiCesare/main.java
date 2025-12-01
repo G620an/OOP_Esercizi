bcd2d0efeac09c5be1eaffe4282a697a9d9d50d9
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 12:40:13 +0100

    update src/CifrarioDiCesare/main.java

--- src/CifrarioDiCesare/main.java
+++ src/CifrarioDiCesare/main.java
@@ -2,9 +2,14 @@ package CifrarioDiCesare;
 import java.IO.*;
 
 public static void main(String[] args){
-  if(args.length != 2){
+  if(args.length != 3){
     throw new IllegalArgumentException("Numero di argomenti diverso da tre");
   }
   String path = args[0];
   String metodo = args[1];
+  int key = Integer.parseInt(args[2]);
+  metodo += metodo.toLower();
+  if(metodo.equals("cifra")){
+    CesarCipher.cifra(path , )
+  }
 }
