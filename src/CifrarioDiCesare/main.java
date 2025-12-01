03763b15663bb30dfe996b6e3d9fe3ed068d001c
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 12:38:15 +0100

    update src/CifrarioDiCesare/main.java

--- src/CifrarioDiCesare/main.java
+++ src/CifrarioDiCesare/main.java
@@ -2,9 +2,9 @@ package CifrarioDiCesare;
 import java.IO.*;
 
 public static void main(String[] args){
-  if(args.length != 3){
+  if(args.length != 2){
     throw new IllegalArgumentException("Numero di argomenti diverso da tre");
   }
   String path = args[0];
-  
+  String metodo = args[1];
 }
