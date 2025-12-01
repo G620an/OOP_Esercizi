6f0293c6f8f6b2f9d789c2ff3c2d2e073b2c4381
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 13:06:08 +0100

    update src/Magazzino/MsgazzinoUtilSerrializable.java

--- src/Magazzino/MsgazzinoUtilSerrializable.java
+++ src/Magazzino/MsgazzinoUtilSerrializable.java
@@ -4,5 +4,11 @@ import java.IO.*;
 public class MagazzinoUtilSerializable{
   private MagazzinoUtilSerializable(){}
   
-  
+  public void salvaMagazzino(String path, Magazzino magazzino){
+    try(BufferedReader bw = new BuferedReader(new FileReader(path))){
+      
+    }catch(IOException e){
+      e.printStackTrace();
+    }
+  }
 } 
