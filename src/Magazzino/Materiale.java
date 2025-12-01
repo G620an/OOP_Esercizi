26318e2edf49cf6023507b563cd37e93b0c9e3fb
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-01 13:04:36 +0100

    update src/Magazzino/Materiale.java

--- src/Magazzino/Materiale.java
+++ src/Magazzino/Materiale.java
@@ -1,5 +1,5 @@
 package Magazino;
-public record Materiale(String nome, int qta){
+public record Materiale(String nome, int qta) implements Serializzable{
   public Materiale{
     if(nome == null || wta < 0){
       throw new IllegalArgumentException();
