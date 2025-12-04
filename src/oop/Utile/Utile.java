5d0ebad35e926402b797a846cb0559cb2a2500a8
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-04 10:34:09 +0100

    update src/oop/Utile/Utile.java

--- src/oop/Utile/Utile.java
+++ src/oop/Utile/Utile.java
@@ -24,7 +24,7 @@ public class Utile{
             f1 = new File(temp1);
             f2 = new File(temp2);
             try(DataOutputStream dos1 = new DataOutputStream(new FileOutputStream(f1)); DataOutputStream dos2 = new DataOutputStream(new FileOutputStream(f2)); DataInputStream dis = new DataInputStream(new FileInputStream(f));){
-                Scaner sc = new Scanner(dos)
+                Scaner sc = new Scanner(dis)
                 boolean primo = true;
                 int pre = Integer.MIN_VALUE;
                 int cor = dis.readInt();
