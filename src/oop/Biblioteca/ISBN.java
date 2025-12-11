4fd6f2b355b6908cfe8253ba8aa11e9560ef1796
Gabriele Iuele <gabriele.iue2@gmail.com>
2025-12-11 10:04:59 +0100

    update src/oop/Biblioteca/ISBN.java

--- src/oop/Biblioteca/ISBN.java
+++ src/oop/Biblioteca/ISBN.java
@@ -2,7 +2,8 @@ Package Biblioteca;
 
 public class ISBN{
   private String isbn;
-  private String regex = "([0-9]{9})";
+  private String regex = "([0-9]{9})(0-9X)";
+  private Pattern p = Pattern.compile(regex);
   
   public ISBN (String isbn){
     
